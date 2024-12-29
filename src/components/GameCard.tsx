fd4d3df7edcabc04cc7f91224fb16eaffc69b93a
RnElite <norakelite@gmail.com>
2024-12-29 10:00:38 +0700

    Displaying Critic Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,24 @@
 import { Game } from "@/hooks/useGame";
-import { Card, CardBody, CardTitle, Image } from "@chakra-ui/react";
+import { Card, CardBody, CardTitle, HStack, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 
 interface Props{
     game: Game;
 }
 
+
 const GameCard = ({game}:Props) => {
   return (
     <Card.Root borderRadius={10} maxW={'sm'} overflow={'hidden'}>
         <Image  src={game.background_image}></Image>
         <CardBody>
-            <CardTitle>{game.name}</CardTitle>
+          <CardTitle>{game.name}</CardTitle> 
+          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}></PlatformIconList>
+          <HStack justifyContent='space-between'>
+            <CriticScore score={game.metacritic}></CriticScore>
+          </HStack>
         </CardBody>
     </Card.Root>
   )
