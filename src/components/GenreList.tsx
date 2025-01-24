59659eb8f3379a81cf135eae149ab009884c562f
RnElite <norakelite@gmail.com>
2025-01-24 12:28:51 +0700

    Clean up the genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,6 @@
 import useGenres, { Genre } from "@/hooks/useGenre";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, Link, Stack } from "@chakra-ui/react";
+import { Heading, HStack, Image, Link, Stack } from "@chakra-ui/react";
 import GenreSkeleton from "./GenreSkeleton";
 
 interface Props {
@@ -12,14 +12,17 @@ const GenreList = ({selectedGenre, onSelectGenre}:Props) => {
   const {data, error, isLoading} = useGenres();
   const skeletons = [1,2,3,4,5,6,7,8,9,10];
   return (
-    <Stack>
-      {isLoading && skeletons.map(skeleton =><GenreSkeleton key={skeleton}></GenreSkeleton>)}
-      {data.map(genre => 
-      <HStack paddingY={1} key={genre.id}>
-          <Image borderRadius={8} src={getCroppedImageUrl(genre.image_background)} alt={genre.name} boxSize="32px" />
-          <Link fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={()=>onSelectGenre(genre)} href="#" fontSize={'lg'}>{genre.name}</Link>
-      </HStack>)}
-    </Stack>
+    <>
+      <Heading size={'2xl'} marginBottom={3}>Genres</Heading>
+      <Stack>
+        {isLoading && skeletons.map(skeleton =><GenreSkeleton key={skeleton}></GenreSkeleton>)}
+        {data.map(genre => 
+        <HStack paddingY={1} key={genre.id}>
+            <Image objectFit={'cover'} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} alt={genre.name} boxSize="32px" />
+            <Link whiteSpace={'normal'} fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={()=>onSelectGenre(genre)} href="#" fontSize={'lg'}>{genre.name}</Link>
+        </HStack>)}
+      </Stack>
+    </>
   )
 }
 
